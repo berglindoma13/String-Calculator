0b59599cf18171f02c1de9b69a3430c63e1eed47
berglindoma13 <berglindoma13@ru.is>
2015-10-12 17:14:19 +0000

    Fixing mistakes

---
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -0,0 +1,14 @@
+package is.ru.stringcalculator;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	public static void main(String args[]) {
+		org.junit.runner.JunitCore.main("is.ru.stringcalculator")
+
+	}
+
+	@Test
+}
